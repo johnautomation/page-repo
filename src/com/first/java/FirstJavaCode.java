7289fecefda072bba4f6d30e47baa9242059af75
johnautomation <yohannes_tg@yahoo.com>
2018-02-12 00:51:01 -0800

    added first.java

---
+++ src/com/first/java/FirstJavaCode.java
@@ -0,0 +1,14 @@
+
+package com.first.java;
+
+public class FirstJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("The First outPut in java Class");
+
+	}
+
+}
